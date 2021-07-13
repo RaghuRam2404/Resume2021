2fea2c8a64d166996f195d1eb7d6649392ab271a
Raghu Ram <raghuram2404@gmail.com>
2021-07-13 17:52:34 +0530

    header & footer moved

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+var index_page = "index.html"
+var about_page = "about.html"
+
 function toggle_menu(){
 	
 	var burger_svg = $(".burger-svg").css('display')
@@ -11,25 +14,9 @@ function toggle_menu(){
 		$(".floatmenu").css('display', 'block')
 	else
 		$(".floatmenu").css('display', 'none')
-
-	/*var state = $(".floatmenu").css('display')
-	if(state == 'block'){
-		$(".floatmenu").css('display', 'none')
-		$(".menuimg img").attr('src','./svg/burgermenu.svg')
-		$(".menuimg").css('margin-top','9.5px')
-		$(".menuimg img").css('margin-right','0px')
-	}
-	else{
-		$(".floatmenu").css('display', 'block')
-		$(".menuimg img").attr('src','./svg/cross.svg')
-		$(".menuimg").css('margin-top','3.5px')
-		$(".menuimg img").css('margin-right','-4px')
-	}*/
 }
 
-function bodyLoad(){
-
-	
+function preloadStuffs(){
 	$('img').each(function(){
 		var img1 = $(this).attr('src')
 		var img2 = $(this).attr('data-hover')
@@ -40,23 +27,58 @@ function bodyLoad(){
 		}
 	}).promise().done(function(){
 		setTimeout(function(){
+
+			$(".svg-change").hover(function(){
+				var ele=this
+				var swap1 = $(ele).attr('src')
+				var swap2 = $(ele).attr('data-hover')
+				$(ele).attr('src',swap2)
+				$(ele).attr('data-hover',swap1)
+			}, function(){
+				var ele = this
+				var swap1 = $(ele).attr('src')
+				var swap2 = $(ele).attr('data-hover')
+				$(ele).attr('src',swap2)
+				$(ele).attr('data-hover',swap1)
+			});
+
+			var parts = window.location.href.split("/")
+			var html_name = parts[parts.length-1]
+			if(html_name == index_page){
+				$(".menuitem:nth-child(1)").addClass("selected")
+				$(".floatmenuitem:nth-child(1) a").addClass("selected")
+			}else if(html_name == about_page){
+				$(".menuitem:nth-child(2)").addClass("selected")
+				$(".floatmenuitem:nth-child(2) a").addClass("selected")
+			}
+
+
 			$(".loading_holder").css("display", "none")
 			$('body').css('background-color', 'white') 
 			$('.body').css('display', 'block') 
 		},1000);
 	})
+}
 
-	$(".svg-change").hover(function(){
-		var ele=this
-		var swap1 = $(ele).attr('src')
-		var swap2 = $(ele).attr('data-hover')
-		$(ele).attr('src',swap2)
-		$(ele).attr('data-hover',swap1)
-	}, function(){
-		var ele = this
-		var swap1 = $(ele).attr('src')
-		var swap2 = $(ele).attr('data-hover')
-		$(ele).attr('src',swap2)
-		$(ele).attr('data-hover',swap1)
+function bodyLoad(){
+
+	var count = 0
+
+	$.ajax({
+		url: "./header.html",
+	    success: function (resp) {
+	    	$('.body').prepend(resp)
+			count += 1
+			if (count == 2){
+				preloadStuffs()
+			}
+	    }
 	});
+
+	$(".footer").load("./footer.html", function(){
+		count += 1
+		if (count == 2){
+			preloadStuffs()
+		}
+	})
 }
